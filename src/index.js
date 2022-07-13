12bacaf163e8abd386f6c4e73faae5b700ad4107
sarthak503 <sarthak.kumar503@gmail.com>
2022-07-13 17:51:02 +0530

    made code readable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// var xlsx = require("xlsx");
-// var wb = xlsx.readFile("../inputfile/sample.xlsx", {});
-// var ws = wb.Sheets["Pre program run"];
-// var data = xlsx.utils.sheet_to_json(ws);
-// console.log(data);
-// //console.log(ws);
-// var ws_data = [["category"]];
 var cate = [
   "Category",
   "SHOPIFY",
@@ -15,15 +8,6 @@ var cate = [
   "OTHERS",
   "MAGENTO",
 ];
-// //var ws1 = xlsx.utils.aoa_to_sheet(ws_data);
-// //let i = 0;
-// // var newData = data.map(function (record) {
-// //   record.category = cate[i];
-// //   i++;
-// // });
-// // // console.log(newData);
-// // wb.Sheets["Pre program run"] = ws1;
-// console.log(ws.getColumn);
 var Excel = require("exceljs");
 var workbook = new Excel.Workbook();
 
